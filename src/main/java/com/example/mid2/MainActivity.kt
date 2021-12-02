5ea92b895cc9d92fac41f21d0b2a5b495ad4a11e
Notnik_kg <ulanovadanaya@gmail.com>
2021-12-02 14:43:41 +0600

    2

--- src/main/java/com/example/mid2/MainActivity.kt
+++ src/main/java/com/example/mid2/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mid2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+      val imageView=findViewById<ImageView>(R.id.s)
+        imageView.setOnClickListener {
+            startActivity(Intent(this,AnimationActivity::class.java))}}}
